c8d349d5aabcc791db3cd07cbac743dfe7bbe52c
ruiter <ruiter@ruiter>
2016-10-04 16:48:17 -0300

    criando style toolbar

--- app/src/main/java/br/com/ruitercomp/mymaterialsamples/MainActivity.java
+++ app/src/main/java/br/com/ruitercomp/mymaterialsamples/MainActivity.java
@@ -1,7 +1,10 @@
 package br.com.ruitercomp.mymaterialsamples;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SimpleToolbar.class));
+            }
+        });
     }
 }
